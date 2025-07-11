e268f2374901d6884779f39b0776f6628f5f63e7
keshav2109-hue <kunalkankani5@gmail.com>
2025-07-12 01:51:21 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,61 @@
+
+/** @type {import('next').NextConfig} */
+const withPWAInit = require('next-pwa');
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development', // Disable PWA in development
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_BASE_URL: 'https://e-leak.vercel.app',
+    NEXT_PUBLIC_TURNSTILE_SITE_KEY: '0x4AAAAAABjZTiJMp4s53ry2',
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dxixtlyravvxx.cloudfront.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.canva.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+module.exports = withPWA(nextConfig);
+
+    
---
+++ next.config.ts
@@ -0,0 +1,2 @@
+// This file has been replaced by next.config.js to resolve a build issue.
+// You can safely delete this file.
